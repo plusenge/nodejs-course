8c3c3893dce36db2a01607bd63d56ee46fbc23a4
plusenge <plusenge01@gmail.com>
2022-09-18 17:21:50 -0600

    Element modified

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-for (let index = 0; index < 4; index++){
-  console.log(index)
-}
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.write("hello node!");
+  res.end()
+})
+const port = 3000;
+server.listen(port, () => {
+  console.log("server listening on port " + port)
+})
