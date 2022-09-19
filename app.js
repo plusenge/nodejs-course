a96da18c8aa317acda1ea2998214edf07176fb7d
plusenge <plusenge01@gmail.com>
2022-09-18 18:09:22 -0600

    text modified

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const http = require("http");
+const { request } = require("https");
 
 const server = http.createServer((req, res) => {
-  res.write("hello node!");
-  res.end()
+  res.write("Hello Nodejs");
+  res.end();
 })
-const port = 3000;
-server.listen(port, () => {
-  console.log("server listening on port " + port)
-})
+
+const port = 3000
+server.listen(port, (err) => {
+  if (port !== 3000) {
+    console.log("Wrong port number")
+  } else {
+    console.log("Server listening on port " + port)
+  }
+ })
